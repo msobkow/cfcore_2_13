fbed380951a2e34ed6a0a692ede3f9025edd4896
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-08-18 23:55:11 +0000

    2020-08-18 MSS Code Factory CFAll 2.13.11195 The tool now builds under Maven for 2.13
    The essential of MSS Code Factory 2.13 have been ported to Maven builds, thanks
    to the assistance of changes made to MSS Code Factory 2.13.11194.
    
    The rules are still out of whack, but it is back to a runnable state after a week
    on hiatus.

--- java/PackageCFCore213.bash
+++ java/PackageCFCore213.bash
@@ -1,15 +1,18 @@
 #!/bin/bash
 #
 Revision=$1
+Build=`echo $Revision | cut --delimiter='-' -f 1`
 if [ ! -d ../installer ]; then
 	mkdir ../installer
 fi
 if [ "$Revision" == "" ]; then
 	echo "ERROR: Revision (argument 1) not specified" 1>&2
 else
-	archiveFile="../installer/cfcore_2_13_${Revision}-java-installer.zip"
+	archiveFile="../installer/cfcore_2_13_${Revision}-DualGPLv3-java-installer.zip"
 	rm -f ${archiveFile}
-	zip -q9 ${archiveFile} ChangeLog lgpl-3.0.txt LICENSE bin/*.jar bin/lib/*.jar bin/lgpl-3.0.txt bin/xsd/*.xsd
+	rm -f bin/*.jar
+	cp ~/.m2/repository/org/msscf/msscf/v2_13/org.msscf.msscf.v2_13.cfcore*/2.13.${Build}/*-2.13.${Build}.jar bin
+	zip -q9 ${archiveFile} ChangeLog *.txt LICENSE bin/*.jar bin/*.txt bin/xsd/*.xsd
 	echo "Packaged ${archiveFile}"
 	ls -l ${archiveFile}
 fi
