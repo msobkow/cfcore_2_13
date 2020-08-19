c7efa369d9a8cf01b7dac7aceffa4f789573e805
Mark Stephen Sobkow <msobkow@sasktel.net>
2020-08-19 09:27:53 +0000

    Make sure the archives specify the DualLGPL for CFCore.

--- java/PackageCFCore213.bash
+++ java/PackageCFCore213.bash
@@ -8,7 +8,7 @@ fi
 if [ "$Revision" == "" ]; then
 	echo "ERROR: Revision (argument 1) not specified" 1>&2
 else
-	archiveFile="../installer/cfcore_2_13_${Revision}-DualGPLv3-java-installer.zip"
+	archiveFile="../installer/cfcore_2_13_${Revision}-DualLGPLv3-java-installer.zip"
 	rm -f ${archiveFile}
 	rm -f bin/*.jar
 	cp ~/.m2/repository/org/msscf/msscf/v2_13/org.msscf.msscf.v2_13.cfcore*/2.13.${Build}/*-2.13.${Build}.jar bin
